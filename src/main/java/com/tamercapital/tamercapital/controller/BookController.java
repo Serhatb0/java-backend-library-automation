51187f616132937733a010332537942ed8811cdc
Serhatb0 <77425377+Serhatb0@users.noreply.github.com>
2021-10-27 19:05:40 +0300

    tamer-capital

--- src/main/java/com/tamercapital/tamercapital/controller/BookController.java
+++ src/main/java/com/tamercapital/tamercapital/controller/BookController.java
@@ -4,15 +4,12 @@ package com.tamercapital.tamercapital.controller;
 import com.tamercapital.tamercapital.business.abstracts.BookService;
 import com.tamercapital.tamercapital.model.Dtos.CreateDtos.BookCreateRequest;
 import com.tamercapital.tamercapital.model.Dtos.UpdateDtos.BookUpdateRequest;
-import com.tamercapital.tamercapital.model.Dtos.ViewDtos.BookViewRequest;
-import com.tamercapital.tamercapital.model.concretes.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
@@ -29,27 +26,26 @@ public class BookController {
     public ResponseEntity<?> findById(@PathVariable String id){
         return  ResponseEntity.ok(this.bookService.findById(id));
     }
-
-    @PutMapping("/book/{bookId}")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    @PutMapping("/{bookId}")
     public ResponseEntity<?> update(@Valid @PathVariable("bookId") String id,@RequestBody BookUpdateRequest bookUpdateRequest){
         return  ResponseEntity.ok(this.bookService.update(id,bookUpdateRequest));
 
     }
 
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+
     @GetMapping()
     public ResponseEntity<?> getAll(){
         return  ResponseEntity.ok(this.bookService.getAll());
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/add")
     public  ResponseEntity<?> add(@Valid @RequestBody BookCreateRequest bookCreateRequest){
         return  ResponseEntity.ok(this.bookService.add(bookCreateRequest));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/book/{bookId}")
+    @DeleteMapping("/delete/{bookId}")
     public  ResponseEntity<?> delete(@PathVariable("bookId") String id){
         this.bookService.delete(id);
         return ResponseEntity.ok().build();
